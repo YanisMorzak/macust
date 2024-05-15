48fc9187af681f22785652a30fc0630a184cd25e
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-05-15 11:45:47 +0200

    🚧   F01-Hero-Section: add conditional check if user are logged and displays a sign out button if it is

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,31 @@
 import Link from 'next/link'
 import MaxWidthWrapper from './MaxWidthWrapper'
+import { buttonVariants } from './ui/button'
 
 const Navbar = async () => {
+  const user = true
 
   return (
-    <nav className='sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-slate-600 bg-slate-800 backdrop-blur-lg transition-all text-white'>
+    <nav className='sticky z-[100] h-14 lg:h-16 inset-x-0 top-0 w-full border-b border-slate-600 bg-slate-800 backdrop-blur-lg transition-all text-white'>
       <MaxWidthWrapper>
-        <div className='flex h-14 items-center justify-between border-b border-slate-600'>
-          <Link href='/' className='flex z-40 font-semibold'>
+        <div className='flex h-14 lg:h-16 items-center justify-between border-b border-slate-600'>
+          <Link href='/' className='flex z-40 font-semibold text-2xl lg:text-3xl'>
             Ma<span className='text-purple-600'>cust</span>
           </Link>
+        <div className='h-full flex items-center space-x-4'>
+        {user ? (
+              <>
+            <Link
+                href='/api/auth/logout'
+                 className={buttonVariants({
+                 size: 'sm',
+                 variant: 'purple',
+                })}>
+                Sign out
+            </Link>
+            </>
+        ): <></>}
+        </div>
         </div>
       </MaxWidthWrapper>
     </nav>
