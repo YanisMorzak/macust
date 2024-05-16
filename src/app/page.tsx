3c4cbe104c29be723f6f426012e52ea50d43633d
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-05-16 09:54:22 +0200

    🚧       F03-Landing-Page: add value proposition section and the first user review

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import MaxWidthWrapper from "@/src/components/MaxWidthWrapper";
 import TemplateMac from "@/src/components/TemplateMac";
 import { Check, Star } from "lucide-react";
 import Image from "next/image";
+import { Icons } from "../components/Icons";
 
 export default function Home() {
   return (
@@ -92,6 +93,58 @@ export default function Home() {
           </div>
         </MaxWidthWrapper>
       </section>
+
+       {/* value proposition section */}
+       <section className='bg-[#452489] grainy-dark py-24'>
+        <MaxWidthWrapper className='flex flex-col items-center gap-16 sm:gap-32'>
+          <div className='flex flex-col lg:flex-row items-center gap-4 sm:gap-6'>
+            <h2 className='order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-white'>
+              What {' '}
+              <span className='bg-gradient-to-r from-slate-800 to-violet-600 rounded-md px-4'>
+              our customers
+              </span>{' '}
+              say
+            </h2>
+        </div>
+
+        <div className='mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0 lg:max-w-none lg:grid-cols-2 gap-y-16'>
+            <div className='flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20'>
+              <div className='flex gap-0.5 mb-2'>
+              <Star className='h-5 w-5 text-purple-500 fill-purple-500' />
+                    <Star className='h-5 w-5 text-purple-500 fill-purple-500' />
+                    <Star className='h-5 w-5 text-purple-500 fill-purple-500' />
+                    <Star className='h-5 w-5 text-purple-500 fill-purple-500' />
+                    <Star className='h-5 w-5 text-purple-500 fill-purple-500' />
+              </div>
+              <div className='text-lg leading-8'>
+                <p>
+                  "The case feels durable and I even got a compliment on the
+                  design. Had the case for two and a half months now and{' '}
+                  <span className='p-0.5 bg-slate-800 text-white'>
+                    the image is super clear
+                  </span>
+                  , on the case I had before, the image started fading into
+                  yellow-ish color after a couple weeks. Love it."
+                </p>
+              </div>
+              <div className='flex gap-4 mt-2'>
+                <img
+                  className='rounded-full h-12 w-12 object-cover'
+                  src='/users/user-1.png'
+                  alt='user'
+                />
+                <div className='flex flex-col'>
+                  <p className='font-semibold'>Jonathan</p>
+                  <div className='flex gap-1.5 items-center text-zinc-200'>
+                    <Check className='h-4 w-4 stroke-[3px] text-purple-600' />
+                    <p className='text-sm'>Verified Purchase</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            </div>
+        </MaxWidthWrapper>
+        </section>
     </div>
   ); 
 }
