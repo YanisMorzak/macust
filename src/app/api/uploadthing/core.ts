8f789165fef3dd5e5a569778a1473d61c8fb5003
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-05-21 10:18:28 +0200

    🚧      F04-Upload-Page:  addition of input data validation and adjustment of data management in middleware and the onUploadComplete function

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,30 +1,22 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next";
-import { UploadThingError } from "uploadthing/server";
+import { z } from 'zod'
  
 // crée une instance de l'outil "uploadthing" en utilisant la fonction createUploadthing() et la stocke dans la variable f
 const f = createUploadthing();
- 
-//  fonction d'authentification factice auth qui simule l'authentification de l'utilisateur. Cette fonction est utilisée pour vérifier les autorisations de téléchargement avant de permettre le téléchargement
-const auth = (req: Request) => ({ id: "fakeId" }); 
  
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
   imageUploader: f({ image: { maxFileSize: "4MB" } })
+  .input(z.object({ configId: z.string().optional() }))
     // Set permissions and file types for this FileRoute
-    .middleware(async ({ req }) => {
+    .middleware(async ({ input }) => {
       // This code runs on your server before upload
-      const user = await auth(req);
- 
-      // If you throw, the user will not be able to upload
-      if (!user) throw new UploadThingError("Unauthorized");
- 
-      // Whatever is returned here is accessible in onUploadComplete as `metadata`
-      return { userId: user.id };
+      return {input}
     })
     .onUploadComplete(async ({ metadata, file }) => {
-     
-      return { uploadedBy: metadata.userId };
+        const { configId } = metadata.input
+        return { configId }
     }),
 } satisfies FileRouter;
  
