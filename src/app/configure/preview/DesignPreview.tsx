ddd5ae1c873abf274610ead420f22c5118d6286e
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-06-23 11:20:20 +0200

    🚧           F06-Configuration-Summary:  add confetti when DesignPreview is mount

--- src/app/configure/preview/DesignPreview.tsx
+++ src/app/configure/preview/DesignPreview.tsx
@@ -1,5 +1,22 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
+import Confetti from "react-dom-confetti";
 
 export default function DesignPreview() {
-  return <div>DesignPreview</div>;
+  const [showConfetti, setShowConfetti] = useState<boolean>(false);
+  useEffect(() => setShowConfetti(true));
+  return (
+    <>
+      <div
+        aria-hidden="true"
+        className="pointer-events-none select-none absolute inset-0 overflow-hidden flex justify-center"
+      >
+        <Confetti
+          active={showConfetti}
+          config={{ elementCount: 200, spread: 90 }}
+        />
+      </div>
+    </>
+  );
 }
