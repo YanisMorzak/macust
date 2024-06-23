09662590b91b3faaa83e3ae306aec93c00950b75
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-06-23 16:47:35 +0200

    🚧           F06-Configuration-Summary:  display additional informations of the customs product

--- src/app/configure/preview/DesignPreview.tsx
+++ src/app/configure/preview/DesignPreview.tsx
@@ -40,15 +40,33 @@ export default function DesignPreview({
             imgSrc={configuration.croppedImageUrl!}
           />
         </div>
-      </div>
-
-      <div className="mt-6 sm:col-span-9 md:row-end-1">
-        <h3 className="text-3xl font-bold tracking-tight text-gray-900">
-          Your {modelLabel} Custom
-        </h3>
-        <div className="mt-3 flex items-center gap-1.5 text-base">
-          <Check className="h-4 w-4 text-green-500" />
-          In stock and ready to ship
+        <div className="mt-6 sm:col-span-9 md:row-end-1">
+          <h3 className="text-3xl font-bold tracking-tight text-gray-900">
+            Your {modelLabel} Custom
+          </h3>
+          <div className="mt-3 flex items-center gap-1.5 text-base">
+            <Check className="h-4 w-4 text-green-500" />
+            In stock and ready to ship
+          </div>
+        </div>
+        <div className="sm:col-span-12 md:col-span-9 text-base ml-2 md:ml-0">
+          <div className="grid grid-cols-1 gap-y-8 border-b border-gray-200 py-8 sm:grid-cols-2 sm:gap-x-6 sm:py-6 md:py-10">
+            <div>
+              <p className="font-medium text-zinc-950">Highlights</p>
+              <ol className="mt-3 text-zinc-700 list-disc list-inside">
+                <li>Wireless charging compatible</li>
+                <li>Packaging made from recycled materials</li>
+                <li>5 year print warranty</li>
+              </ol>
+            </div>
+            <div>
+              <p className="font-medium text-zinc-950">Materials</p>
+              <ol className="mt-3 text-zinc-700 list-disc list-inside">
+                <li>High-quality, durable material</li>
+                <li>Scratch- and fingerprint resistant coating</li>
+              </ol>
+            </div>
+          </div>
         </div>
       </div>
     </>
